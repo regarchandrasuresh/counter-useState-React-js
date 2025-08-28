18c6b863c64dfb6baf904fc80069547b6a9b6c4e
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-08-28 11:32:27 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+import "./style.css";
+import Counter from "./Counter.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
